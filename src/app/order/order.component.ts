1a72ba5914e3111a6dee27f6898bfbe2e926f664
GSGranados <stevengranados0211@gmail.com>
2020-08-23 20:48:18 -0600

    Base Logic Created to submit an order

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,17 +4,59 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
 })
 export class OrderComponent implements OnInit {
+  onionQ: number = 0;
+  lettuceQ: number = 0;
+  chesseQ: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  orderBurger(OrderForm:NgForm){
+  orderBurger(OrderForm: NgForm) {
     console.log('Este metodo manda algo!');
+    if(OrderForm.value.burgerName === ""){
+      OrderForm.value.burgerName = "Placeholder Burger"
+    }
+    const BurgerOrder: object = {
+      name: OrderForm.value.burgerName,
+      cost : this.calculateCost(0 , this.onionQ, this.lettuceQ, this.chesseQ),
+      onions: this.onionQ,
+      lettuces: this.lettuceQ,
+      cheeses: this.chesseQ,
+    }
+
+    console.log(BurgerOrder);
   }
 
+  //INCREASE/DECREASE METHODS
+  increaseOnion() {
+    this.onionQ = this.onionQ + 1;
+  }
+
+  decreaseOnionQ() {
+    this.onionQ = this.onionQ - 1;
+  }
+  increaseLettuceQ() {
+    this.lettuceQ = this.lettuceQ + 1;
+  }
+
+  decreaseLettuceQ() {
+    this.lettuceQ = this.lettuceQ - 1;
+  }
+
+  increaseCheeseQ(){
+    this.chesseQ = this.chesseQ +1 ;
+  }
+
+  decreaseCheeseQ(){
+    this.chesseQ = this.chesseQ -1;
+  }
+  // END OF INCREASE/DECREASE METHODS
+
+  calculateCost(basePrice:number, onionQ:number, lettuceQ:number, cheeseQ:number){
+    return basePrice + (onionQ*1.2) + (lettuceQ*0.50 ) + (cheeseQ*0.75);
+  }
 }
